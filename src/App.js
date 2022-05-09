8bf96e589bc5fbde20e0ca53eed7653ab79306d1
AlinaThakur <poojathakur8746@gmail.com>
2022-05-09 16:42:54 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import SimpleInput from './components/SimpleInput';
+import Basicform from './components/BasicForm'
+function App() {
+  return (
+    <div className="app">
+      <Basicform/>
+    </div>
+  );
+}
+
+export default App;
